3f732e554384fd6a67a09055d6f74276e39b8103
dmerz75 <merz_drm@hotmail.com>
2013-03-05 22:52:13 -0500

    added whole directory

---
+++ 00.maindir.amb.asmd/VDIR/go-multisndr.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+import sys,os,re,shutil,time
+from glob import glob
+import numpy as np
+from random import randint
+
+JOBID = os.environ['PBS_JOBID'].split('.')[0]
+
+my_dir=os.path.abspath(os.path.dirname(__file__))
+num   =my_dir.split('/')[-2]
+prev_num=str(int(num)-1).zfill(2)
+predir1='/'.join(my_dir.split('/')[0:-1])
+predir2='/'.join(my_dir.split('/')[0:-2])
+cfile =os.path.join(predir2,'%s-continue.py' % num)
+
+howmany= xxhowmanyxx
+quota  = xxquotaxx*xxhowmanyxx
+t_data = np.array([])
+slist  = []
+
+def cp_file(f_dir,f,d_dir,d):
+    shutil.copy(os.path.join(f_dir,f),os.path.join(d_dir,d))
+
+def reg_ex(script,subout,subin,n):
+    o=open(script,'r+')
+    text=o.read()
+    text=re.sub(subout,subin,text)
+    o.close()
+    o=open(script,'w+')
+    o.write(text)
+    o.close()
+
+def run_amb(i):
+    seed = randint(10000,99999)
+    while seed in slist:
+        seed = randint(10000,99999)
+    slist.append(seed)
+    cp_file(my_dir,'smd.in',my_dir,'smd.in.%s' % (seed))
+    script = os.path.join(my_dir,'smd.in.%s' % (seed))
+    reg_ex(script,'xxxxx',str(seed),i)
+    st = time.time()
+    os.system('mpirun -np xxnodecountxx \
+              -machinefile $PBS_NODEFILE ${AMBERHOME}/bin/sander -O \
+              -i smd.in.%s \
+              -o run.log \
+              -c ../../../../xxstrucequilxx/00.inpcrd \
+              -p ../../../../xxstrucequilxx/00.prmtop \
+              -r md.rst.%s \
+              -x md.mdcrd \
+              -v md.mdvel' % (seed,seed))
+    tt = time.time()-st
+    os.system("mv dist_vs_t %d-tef.dat.%s" % (i,seed))
+    os.system("python ../hb.py %d %s" % (i,seed))
+    return tt
+
+def next_stage(dir_loc,cfile):
+    os.chdir(dir_loc)
+    os.system('python %s %s' % (cfile,prev_num))
+
+def check_vel(pnum):
+    if pnum=='00':
+        pass
+    else:
+        cnt = 0
+        while os.path.isfile(os.path.join(predir1,'00.rst'))==False:
+            next_stage(predir2,cfile)
+            cnt += 1
+            print "waiting 20 seconds ..."
+            print cnt
+            time.sleep(20)
+            if cnt >= 20:
+                sys.exit()
+    os.chdir(my_dir)
+
+check_vel(prev_num)
+
+for i in range(1,howmany+1):
+    t1 = run_amb(i)
+    t_data = np.append(t_data,t1)
+    if i%howmany==0:
+        np.savetxt('time.dat',t_data,fmt='%.4f')
---
+++ 00.maindir.namd.asmd/VDIR/go-fgatecpu.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+import sys,os,time
+from glob import glob
+import numpy as np
+#import datetime
+import time
+from random import randint
+
+JOBID = os.environ['PBS_JOBID'].split('.')[0]
+#now=datetime.datetime.now().strftime("%m%dt%H%M")
+
+my_dir=os.path.abspath(os.path.dirname(__file__))
+num   =my_dir.split('/')[-2]
+predir1='/'.join(my_dir.split('/')[0:-1])
+predir2='/'.join(my_dir.split('/')[0:-2])
+cfile =os.path.join(predir2,'%s-continue.py' % num)
+
+howmany= xxhowmanyxx
+quota  = xxquotaxx
+t_data = np.array([])
+
+def run_namd(i):
+    st = time.time()
+    os.system('namd2 +pxxnodecountxx smd.namd > run.log')
+    tt = time.time()-st
+    os.system('mv smdforces.out %d-tef.dat.%s' % (i,JOBID))
+    os.system('python ../hb.py %d %s' % (i,JOBID))
+    return tt
+
+for i in range(1,howmany+1):
+    t1 = run_namd(i)
+    t_data = np.append(t_data,t1)
+    if i%howmany==0:
+        np.savetxt('time.dat',t_data,fmt='%.4f')
+
+def next_stage(dir_loc,cfile):
+    os.chdir(dir_loc)
+    os.system('python %s' % cfile)
+
+
+count=0
+for path in glob(os.path.join(predir1,'*/*-tef.dat*')):
+    count+=1
+if count==quota:
+    slp=randint(12,39)
+    time.sleep(slp)
+    next_stage(predir2,cfile)
+elif count!=quota: sys.exit()
+
+#os.system('rm *.BAK')
+#os.system('rm da_smd.coor')
+#os.system('rm da_smd.dcd')
+#os.system('rm da_smd.vel')
+os.system('rm *.xsc')
+os.system('rm run.log')
