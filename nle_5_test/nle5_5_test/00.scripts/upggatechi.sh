d67f095441da114d7a14bcab33e6f0a79239593c
Hailey Bureau <hailey.bureau [@] gmail.com>
2013-03-27 16:36:22 -0400

    removed nel and nle

--- nle_5_test/nle5_5_test/00.scripts/upggatechi.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# rsync ggate
-
-SOURCEDIR=/home/dale/Documents/valiant/workgen/namdgen/chi4/
-DESTDIR=cukachukwu3@ggate.chemistry.gatech.edu:/nethome/cukachukwu3/
-
-#rsync -avh $SOURCEDIR $DESTDIR
-scp -r $SOURCEDIR $DESTDIR
--- nle_5_test/nle5_5_test/tex_nle5_5_test/pdflatex.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# pdflatex 
-
-FILENAME=tm
-
-pdflatex $FILENAME
-bibtex $FILENAME
-pdflatex $FILENAME
-pdflatex $FILENAME
