7f2abcac8b35949a2ea405c1bf68e05514a7810c
Hailey Bureau <hailey.bureau [@] gmail.com>
2013-04-12 11:05:11 -0400

    added chichi stuff

---
+++ ndanvt_100at1/ndanvt5_100at1/03.exp/04/04-jobh.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#PBS -N danvtncjob04
+#PBS -j oe
+#PBS -l walltime=27:00
+#PBS -l pmem=310mb
+#PBS -l nodes=1:ppn=1
+#PBS -V
+
+# job properties
+cd $PBS_O_WORKDIR
+
+NUM=04
+
+# run job
+./00-hb_pkl.py $NUM
